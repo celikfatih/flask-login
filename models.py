41795ce82e1786cf1ef2f4c2c36c6a1d19666236
ce.fatihcelik@gmail.com <ce.fatihcelik@gmail.com>
2020-05-26 18:24:14 +0300

    flask login example with Flask-Login package

---
+++ models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Author 2019 @fatihcelik
+
+from flask_login import UserMixin
+from . import db
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
